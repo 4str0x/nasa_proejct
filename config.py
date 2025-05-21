09d5b5bf37bcfe1a9ad05a43b7e78ee95eaf02b1
Erick Santos <ericksantosyb@gmail.com>
2025-05-21 15:30:42 -0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class _Settings(BaseSettings):
-    # Configurações do JWT
-    API_KEY: str
-    API_URL: str
-
-Settings = _Settings(_env_file=".env")  # type: ignore
